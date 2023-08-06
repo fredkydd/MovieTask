1a69bc4796211d522faf55f55d780aa1da77d00f
Fred Kydd <17003685+fredkydd@users.noreply.github.com>
2023-08-07 02:31:49 +0400

    trying to make slide in action

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const $ = (element) => document.querySelector(element);
+const $$ = (elements) => document.querySelectorAll(elements);
+
+const slideTop = [...$$('article')];
+
+$('.leftTopIcon').addEventListener('click', () => {
+  const [article0, ...articles] = slideTop;
+  article0.style.display = 'none';
+  articles.map((article) => (article.style.display = 'grid'));
+});
+
+$('.rightTopIcon').addEventListener('click', () => {
+  const [article0, ...articles] = slideTop;
+  article0.style.display = 'grid';
+  articles.map((article) => (article.style.display = 'none'));
+});
+
+$('.leftBottomIcon').addEventListener('click', () => {});
+$('.rightBottomIcon').addEventListener('click', () => {});
